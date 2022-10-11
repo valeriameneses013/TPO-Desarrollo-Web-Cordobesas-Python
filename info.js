a31e0cb0b02e242c43df90ff7af391a2185e1993
Paula Sanchez | Grupo Absa <paula.sanchez@grupoabsa.com.ar>
2022-10-10 23:28:30 -0300

    materias con js

--- info.js
+++ info.js
@@ -1,53 +1,104 @@
 /* INFORMACION */
 
-var data = [
+var data= [
 {
     "id": 1,
-    "name": "Matemática"
-    "day": "Lunes, Miércoles y Viernes"
-    "hour": "Matutino y Vespertino"
-    "type": "Virtual"
-    "price" "$500 por 1:30hs"
+    "name": "Matemática",
+    "day": "Lunes, Miércoles y Viernes",
+    "hour": "Mañana y tarde",
+    "type": "Virtual",
+    "price": "$500 por 1:30hs"
 },
 {
     "id": 2,
-    "name": "Química"
-    "day": "Martes y Jueves"
-    "hour": "Por la tarde"
-    "type": "Presencial"
-    "price" "700 por 1:30hs"
+    "name": "Química",
+    "day": "Martes y Jueves",
+    "hour": "Tarde",
+    "type": "Presencial",
+    "price": "$700 por 1:30hs"
 },
 {
     "id": 3,
-    "name": "Física"
-    "day": "Miércoles y sábados"
-    "hour": "Matutino y Vespertino"
-    "type": "Virtual"
-    "price" "$500 por 1:30hs"
+    "name": "Física",
+    "day": "Miércoles y sábados",
+    "hour": "Mañana y tarde",
+    "type": "Virtual",
+    "price": "$500 por 1:30hs"
 },
 {
     "id": 4,
-    "name": "Universidad"
-    "day": "Todos los días"
-    "hour": "Por la tarde"
-    "type": "Presencial"
-    "price" "$1500 por 1:30hs"
+    "name": "Universidad",
+    "day": "Todos los días",
+    "hour": "Tarde",
+    "type": "Presencial",
+    "price": "$1500 por 1:30hs"
 },
+{
+    "id": 5,
+    "name": "Inglés",
+    "day": "Martes y Viernes",
+    "hour": "Mañana y tarde",
+    "type": "Presencial o Virtual",
+    "price": "$500 por 1:30hs"
+},
+{
+    "id": 6,
+    "name": "Lengua",
+    "day": "Todos los días",
+    "hour": "Por la tarde",
+    "type": "Presencial o Virtual",
+    "price": "$400 por 1:30hs"
+},
+
 ]
+
+/*FUNCION =RICK&MORTY */
 
 let cad=`<div class="container">`
 for(materia of data){
     cad+=`<div class="tarjeta">
-     <h2> Materia: ${materia.name}</h2>
+     <h3>  ${materia.name}</h3>
      <div class="cuerpo">
-        <p>Días:${materia.day}</p>
-        <p>Horario:${materia.hour}</p>
+        <p>Días: ${materia.day}</p>
+        <p>Horario: ${materia.hour}</p>
         <p>Modalidad ${materia.type}</p>
-        <p>Valor:${materia.price}</p>    
+        <p>Valor: ${materia.price}</p>   
+        <button class="anotar"></button>
      </div> 
+
     </div>    
     `
 }
 cad+=` </div> `
 console.log(cad)
 document.getElementById("materias").innerHTML=cad
+
+
+/* FUNCION BOTON - REVISAR */
+var x = document.getElementsByClassName("anotar")
+x[0].innerHTML = ` 
+<a href="./registro.html" target="_blank">INSCRIBIRSE</a>
+ `
+ 
+var x = document.getElementsByClassName("anotar")
+x[1].innerHTML = ` 
+<a href="./registro.html" target="_blank">INSCRIBIRSE</a>
+`   
+
+var x = document.getElementsByClassName("anotar")
+x[2].innerHTML = ` 
+<a href="./registro.html" target="_blank">INSCRIBIRSE</a>
+`
+ 
+var x = document.getElementsByClassName("anotar")
+x[3].innerHTML = ` 
+<a href="./registro.html" target="_blank">INSCRIBIRSE</a>
+`   
+var x = document.getElementsByClassName("anotar")
+x[4].innerHTML = ` 
+<a href="./registro.html" target="_blank">INSCRIBIRSE</a>
+`   
+var x = document.getElementsByClassName("anotar")
+x[5].innerHTML = ` 
+<a href="./registro.html" target="_blank">INSCRIBIRSE</a>
+`   
